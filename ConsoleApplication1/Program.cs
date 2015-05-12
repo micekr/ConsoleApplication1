1d260e0052e27667e66dab3176da46efb134e0b8
micekr <micekr@gmail.com>
2015-05-12 14:12:15 -0400

    made a change v2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("some data");
+            Console.WriteLine("Hi.. v2 some data");
 
         }
     }
